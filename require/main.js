a050b15e3e8ed5c95c8d92e38f5c53efa575da14
Jukin4 <jukin4@hotmail.com>
2015-11-17 14:07:05 +0100

    Deleted

--- require/main.js
+++
@@ -1,8 +0,0 @@
-require(["jquery", "jquery-fancybox", "bootstrap-datepicker",
-	"aui", "colpick", "datatables", "tinymce-config",
-	"datepicker-config", "rating", "seleccion-material", 
-	"yui-utilities", "yui-dragdrop", "yui-dragdrop-custom", 
-	"yui-animation", "yui-yahoo"], function ($) {
-    // Dependencies are loaded...
-    // Execute code
-});
